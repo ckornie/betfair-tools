238b6d63e0b955af957c28cf685da4bc8cf4f733
Clancy Kornie <91301915+ckornie@users.noreply.github.com>
2025-11-29 19:24:31 +1100

    Support pagination

--- orders.py
+++ orders.py
@@ -1,45 +1,77 @@
 import argparse
-import betfairlightweight
+import pathlib
 import datetime
+import logging
+import traceback
+import sys
+from typing import Final
 import json
-import tempfile
-def get_cleared_orders(username, application, session_token):
+import betfairlightweight
+
+logger: Final[logging.Logger] = logging.getLogger(__name__)
+count: Final[int] = 1000
+
+
+def cleared_orders(
+        days: int,
+        client: betfairlightweight.Betting,
+):
+    _yesterday = datetime.datetime.now() - datetime.timedelta(days=2)
+    _to = datetime.datetime.combine(_yesterday.date(), datetime.time.min)
+
+    while days >= 0:
+        _ongoing: bool = True
+        _record: int = 0
+        _errors: int = 0
+
+        _from = _to - datetime.timedelta(hours=24)
+        logger.info(f"Requesting cleared orders from {_from:%Y-%m-%dT%H:%M} to {_to:%Y-%m-%dT%H:%M}")
+
+        while _ongoing and _errors < 5:
+            try:
+                response = client.list_cleared_orders(
+                    settled_date_range={
+                        'from': _from.strftime("%Y-%m-%dT%H:%M:%SZ"),
+                        'to': _to.strftime("%Y-%m-%dT%H:%M:%SZ")
+                    },
+                    include_item_description=True,
+                    from_record=_record,
+                    record_count=count,
+                )
+
+                _ongoing = response.more_available
+                _record = _record + len(response.orders)
+                _errors = 0
+
+                logger.debug(f"Recieved {_record} orders")
+
+                yield response
+            except Exception as exception:
+                logger.warning(f"An error occurred: {exception}")
+                _errors = _errors + 1
+        _to = _from
+        days = days - 1
+
+
+def archive_cleared_orders(
+        username: str,
+        application: str,
+        token: str,
+        days: int,
+        archive: pathlib.Path,
+):
     trading = betfairlightweight.APIClient(
         username=username,
         password="placeholder",
         app_key=application,
     )
 
-    try:
-        trading.set_session_token(session_token)
-        print(f"Client initialized with session token for user: {username}")
+    trading.set_session_token(token)
+    logger.info(f"Client initialized with session token for user: {username}")
 
-        _from = datetime.datetime(2025, 11, 20, 12, 0)
-        _to = _from + datetime.timedelta(hours=2)
-
-        print(f"Requesting cleared orders from {_from:%Y_%m-%dT%H:%M} to {_to:%Y_%m-%dT%H:%M}")
-
-        cleared_orders = trading.betting.list_cleared_orders(
-            bet_status="SETTLED",
-            settled_date_range={
-                'from': _from.strftime("%Y-%m-%dT%H:%M:%SZ"),
-                'to': _to.strftime("%Y-%m-%dT%H:%M:%SZ")
-            },
-            include_item_description=True,
-            record_count=100,
-        )
-
-        print(f"Found {len(cleared_orders.orders)} cleared orders.")
-
-        with tempfile.NamedTemporaryFile(mode='w+t', delete=False, prefix="betfair-") as handle:
-            print(f"Writing temporary file: {handle.name}")
-            json.dump(cleared_orders._data, handle)
-
-    except Exception as exception:
-        print(f"An error occurred: {exception}")
-
-    finally:
-        pass
+    with open(archive, "a+t") as file:
+        for response in cleared_orders(days, trading.betting):
+            json.dump(response._data, file)
 
 
 def main():
@@ -48,22 +80,64 @@ def main():
         description="Retrieve cleared Betfair orders using a pre-existing session token."
     )
     parser.add_argument(
-        '-u', '--username', required=True,
-        help='Your Betfair account username.'
+        "-u",
+        "--username",
+        required=True,
+        help="Your Betfair account username."
     )
     parser.add_argument(
-        '-a', '--application', required=True,
-        help='Your Betfair Application Key (App Key).'
+        "-k",
+        "--key",
+        required=True,
+        help="Your Betfair application key."
     )
     parser.add_argument(
-        '-t', '--token', required=True,
-        help='Your existing Betfair session token (ssoid).'
+        "-t",
+        "--token",
+        required=True,
+        help="Your existing Betfair session token."
+    )
+    parser.add_argument(
+        "-a",
+        "--archive",
+        required=True,
+        help="The archive file."
+    )
+    parser.add_argument(
+        "-d",
+        "--days",
+        required=True,
+        type=int,
+        help="The number of days to retreive."
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        required=False,
+        type=bool,
+        default=False,
+        help="Enable verbose logging."
     )
 
-    args = parser.parse_args()
+    arguments = parser.parse_args()
 
-    # Run the main function with the parsed arguments
-    get_cleared_orders(args.username, args.application, args.token)
+    if arguments.verbose:
+        logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
+    else:
+        logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+
+    archive_cleared_orders(
+        arguments.username,
+        arguments.key,
+        arguments.token,
+        arguments.days,
+        pathlib.Path(arguments.archive),
+    )
+
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as exception:
+        logger.warning(f"An error occurred: {exception}")
+        traceback.print_exc(file=sys.stdout)
